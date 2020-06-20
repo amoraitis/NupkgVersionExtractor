dde59cea135402aac6551c49bb55ea400d597569
Anastasios Moraitis <anas.moraitis@gmail.com>
2020-06-20 23:34:06 +0300

    Add project files.

---
+++ NupkgVersionExtractor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NupkgVersionExtractor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] == null || File.Exists(args[0]) == false)
+            {
+                throw new ArgumentException("1st argument should be a valid path to a file.");
+            }
+
+            const string pattern = @"^(.*?)\.(?=(?:[0-9]+\.){2,}[0-9]+(?:-[a-z]+)?\.nupkg)(.*?)\.nupkg$";
+            RegexOptions options = RegexOptions.Singleline;
+
+            foreach (Match m in Regex.Matches(args[0], pattern, options))
+            {
+                var packageVersion = m.Groups?.Values?.Last()?.Value;
+
+                Console.WriteLine(packageVersion ?? string.Empty);
+            }
+        }
+    }
+}
